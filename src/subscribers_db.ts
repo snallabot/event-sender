cdf12ed2b418053603143404983a7e813cfbb110
Sahith Nallapareddy <sahith.reddy@gmail.com>
2024-04-09 17:21:18 -0400

    debug

--- src/subscribers_db.ts
+++ src/subscribers_db.ts
@@ -29,7 +29,7 @@ function FirebaseSubscribersDB(db: Firestore): SubscribersDB {
         },
         async query(eventType: string) {
             const subs = await db.collection("subscibrers").where("events", "array-contains", eventType).get()
-            console.log(subs)
+            console.log(subs.docs)
             return subs.docs.map(d => d.data() as SubscriberLocation)
         }
 
