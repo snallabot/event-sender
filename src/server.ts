889c93d9ca2c6331a42c3f57f11020e4a1cf9c92
Sahith Nallapareddy <sahith.reddy@gmail.com>
2024-04-09 17:08:22 -0400

    debug

--- src/server.ts
+++ src/server.ts
@@ -70,6 +70,7 @@ router.post("/subscribe", async (ctx) => {
             const { delivery, ...snallabotEvent } = incomingEvent
             await eventDB.appendEvent(snallabotEvent)
         }
+        console.log(incomingEvent)
         const subscribers = await subcribersDB.query(incomingEvent.event_type)
         console.log(subscribers)
         const strongConsistency = subscribers.filter(s => s.consistency === SubscriberConsistency.STRONG)
